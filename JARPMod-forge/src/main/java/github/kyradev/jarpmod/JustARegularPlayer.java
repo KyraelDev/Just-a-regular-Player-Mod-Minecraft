4ff379d97b6da9411bb08dd43e40912e33883c9f
Marcello A. Licciardello (KyraDev) <kyrael.dev@gmail.com>
2025-06-19 19:43:00 +0200

    Init

---
+++ JARPMod-forge/src/main/java/github/kyradev/jarpmod/JustARegularPlayer.java
@@ -0,0 +1,11 @@
+package github.kyradev.jarpmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(JustARegularPlayer.MODID)
+public class JustARegularPlayer {
+    public static final String MODID = "jarpmod";
+
+
+}
